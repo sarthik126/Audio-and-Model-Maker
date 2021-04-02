15e3b99c5eba3f0764c0b56081f2d0f2d85ed34c
sarthik126 <65937746+sarthik126@users.noreply.github.com>
2021-04-02 19:43:55 +0530

    Update model_creater.py

--- model_creater.py
+++ model_creater.py
@@ -2,9 +2,7 @@ import librosa
 import soundfile
 import os, glob, pickle
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
 
 #DataFlair - Extract features (mfcc, chroma, mel) from a sound file
 def extract_feature(file_name, mfcc, chroma, mel):
